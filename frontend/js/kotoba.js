18b2970f3bbf786cdbd22a51dc00409b724a4a51
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2018-09-18 23:30:36 +0300

    Add equivalent to jQuery $.on() function

--- frontend/js/kotoba.js
+++ frontend/js/kotoba.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill';
 import $ from 'jquery';
 import 'jquery-serializejson';
 
---
+++ frontend/js/utils/event-utils.js
@@ -0,0 +1,35 @@
+function* parentNodeGenerator(rootElement, targetElement) {
+  if (!rootElement.contains(targetElement)) {
+    throw new Error('rootElement is not an ancestor of targetElement');
+  }
+  while (targetElement) {
+    yield targetElement;
+    targetElement = targetElement.parentNode;
+    if (targetElement === rootElement) {
+      yield targetElement;
+      return;
+    }
+  }
+}
+
+
+const addListener = (type, target, selector, listener) => {
+  target.addEventListener(type, (e) => {
+    const parentNodes = parentNodeGenerator(target, e.target);
+    for (let t of parentNodes) {
+      if (!selector || t.matches(selector)) {
+        listener.call(t, e);
+        break;
+      }
+    }
+  });
+}
+
+
+const on = (types, target, selector, listener) => {
+  types
+    .split(' ')
+    .forEach((type) => addListener(type, target, selector, listener));
+};
+
+export { on };
