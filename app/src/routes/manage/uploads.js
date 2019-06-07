67de82be9173393f814bb2fc6aa8a3bfa6239f40
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2019-06-07 03:12:15 +0300

    Add boards filter to /manage/uploads

--- app/src/routes/manage/uploads.js
+++ app/src/routes/manage/uploads.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const Post = require('../../models/post');
+const Board = require('../../models/board');
+
 
 const router = express.Router();
 
 router.get('/uploads',
   async (req, res, next) => {
     try {
+      let availableBoardsQuery = {};
+      if (req.user.authority !== 'admin') {
+        availableBoardsQuery = {
+          boardUri: {
+            $in: Array.from(req.user.boardRoles.keys()),
+          }
+        };
+      }
+      const boards = await Board.find(availableBoardsQuery, 'uri -_id' );
+
+      let selectedBoards = boards;
+      const boardsFilter = req.query.filter;
+      if (boardsFilter && boardsFilter.length) {
+        selectedBoards = selectedBoards.filter((b) => {
+          return boardsFilter.includes(b.uri);
+        });
+      }
+
       const uploadsLimit = 100;
       const posts = await Post
         .find(
-          {'attachments.0': { $exists: true }},
-          {'attachments': 1, 'boardUri': 1, 'postId': 1, 'threadId': 1, 'timestamp': 1}
+          {
+            boardUri: {
+              $in: selectedBoards.map(b => b.uri),
+            },
+            'attachments.0': {
+              $exists: true
+            }
+          },
+          {
+            attachments: 1,
+            boardUri: 1,
+            postId: 1,
+            threadId: 1,
+            timestamp: 1
+          }
         )
         .sort({'timestamp': -1})
         .limit(uploadsLimit);
@@ -18,6 +51,8 @@ router.get('/uploads',
         activity: 'manage-page-upoads',
         title: 'Recent uploads',
         posts: posts,
+        boards: boards,
+        selectedBoards: selectedBoards,
       });
     } catch(err) {
       next(err);
