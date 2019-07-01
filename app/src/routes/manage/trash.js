99c90fad35638259a23410b532adf17a7143f156
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2019-07-01 03:11:12 +0300

    Show deleted posts on manage/trash

--- app/src/routes/manage/trash.js
+++ app/src/routes/manage/trash.js
@@ -1,12 +1,101 @@
 const express = require('express');
 const Asset = require('../../models/asset');
+const Post = require('../../models/post');
+const Board = require('../../models/board');
 
 const router = express.Router();
 
-router.get('/trash/',
+router.get('/trash',
   async (req, res, next) => {
     try {
+      let availableBoardsQuery = {};
+      if (req.user.authority !== 'admin') {
+        availableBoardsQuery = {
+          boardUri: {
+            $in: Array.from(req.user.boardRoles.keys()),
+          }
+        };
+      }
+
+      const boards = await Board.find(availableBoardsQuery, 'uri -_id' );
+      const postsQuery = {
+        boardUri: {
+          $in: boards.map(b => b.uri),
+        },
+        isDeleted: true,
+      };
+
       const deletedAssets = await Asset.find({ isDeleted: true }).exec();
+      const deletedPosts = await Post.find(postsQuery).sort({'timestamp': -1});
+
+      res.render('manage/trash', {
+        activity: 'manage-page-trash',
+        numberOfDeletedAssets: deletedAssets.length,
+        numberOfDeletedPosts: deletedPosts.length,
+        title: 'Recycle Bin'
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get('/trash/posts',
+  async (req, res, next) => {
+    try {
+      let availableBoardsQuery = {};
+      if (req.user.authority !== 'admin') {
+        availableBoardsQuery = {
+          boardUri: {
+            $in: Array.from(req.user.boardRoles.keys()),
+          }
+        };
+      }
+      const boards = await Board.find(availableBoardsQuery, 'uri -_id' );
+
+      let selectedBoards = boards;
+      const boardsFilter = req.query.filter;
+      if (boardsFilter && boardsFilter.length) {
+        selectedBoards = selectedBoards.filter((b) => {
+          return boardsFilter.includes(b.uri);
+        });
+      }
+
+      const postsQuery = {
+        boardUri: {
+          $in: selectedBoards.map(b => b.uri),
+        },
+        isDeleted: true,
+      };
+
+      const postsLimit = 100;
+      const posts = await Post
+        .find(postsQuery)
+        .sort({'timestamp': -1})
+        .limit(postsLimit);
+
+      const deletedAssets = await Asset.find({ isDeleted: true }).exec();
+      const deletedPosts = await Post.find({ isDeleted: true }).exec();
+
+      res.render('manage/trash', {
+        activity: 'manage-page-trash',
+        deletedPosts: posts,
+        boards: boards,
+        selectedBoards: selectedBoards,
+        title: 'Recycle Bin'
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get('/trash/assets',
+  async (req, res, next) => {
+    try {
+      
+      const deletedAssets = await Asset.find({ isDeleted: true }).exec();
+      const deletedPosts = await Post.find({ isDeleted: true }).exec();
 
       res.render('manage/trash', {
         activity: 'manage-page-trash',
