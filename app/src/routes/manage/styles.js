e11493d23255be9a935d3fc0ea674b8e0c8e9d1f
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2019-10-04 03:28:31 +0300

    Change /manage/styles routes

--- app/src/routes/manage/styles.js
+++ app/src/routes/manage/styles.js
@@ -6,32 +6,70 @@ const { authRequired } = require('../../middlewares/permission');
 
 const router = express.Router();
 
-router.get('/styles/:stylename?',
+
+router.get('/styles/',
   authRequired,
   async (req, res, next) => {
     try {
-      const styles = await Style.findAll();
-      let style = null;
-      if (req.params.stylename) {
-        style = await Style.findByName(req.params.stylename);
-        if (!style) {
-          return res.redirect('/manage/styles/');
-        }
-      }
       const initialValues = Object.assign({},
         defaultSyles.find(o => o.name === 'futaba'));
       initialValues.name = '';
       res.render('manage/styles', {
         activity: 'manage-page-styles',
-        styles: styles,
-        style: style,
         initialValues: initialValues,
         title: 'Site styles',
+        crud: 'read',
       });
     } catch (err) {
       next(err);
     }
   }
 );
+
+
+router.get('/styles/edit/:stylename',
+  authRequired,
+  async (req, res, next) => {
+    try {
+      let style = await Style.findByName(req.params.stylename);
+      if (!style) {
+        return res.redirect('/manage/styles/');
+      }
+      const initialValues = Object.assign({},
+        defaultSyles.find(o => o.name === 'futaba'));
+      initialValues.name = '';
+      res.render('manage/styles', {
+        activity: 'manage-page-styles',
+        style: style,
+        initialValues: initialValues,
+        title: 'Site styles',
+        crud: 'update',
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+
+router.get('/styles/create',
+  authRequired,
+  async (req, res, next) => {
+    try {
+      const initialValues = Object.assign({},
+        defaultSyles.find(o => o.name === 'futaba'));
+      initialValues.name = '';
+      res.render('manage/styles', {
+        activity: 'manage-page-styles',
+        initialValues: initialValues,
+        title: 'Site styles',
+        crud: 'create',
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
 
 module.exports = router;
