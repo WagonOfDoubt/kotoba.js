9b1e3296ac6007d6fb6bef18406e3d783c76cc06
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2019-10-11 17:17:28 +0300

    Change manage/news routes

--- app/src/routes/manage/news.js
+++ app/src/routes/manage/news.js
@@ -6,32 +6,55 @@ const { authRequired } = require('../../middlewares/permission');
 
 const router = express.Router();
 
-router.get('/news/:newsId?',
+router.get('/news/',
   authRequired,
-  param('newsId').optional().isNumeric(),
+  async (req, res, next) => {
+    try {
+      const newsList = await News.find().sort({ createdAt: -1 });
+      res.render('manage/news', {
+        activity: 'manage-page-editnews',
+        newsList: newsList,
+        title: 'News',
+        crud: 'read',
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get('/news/edit/:newsId',
+  authRequired,
+  param('newsId').isNumeric(),
   validateRedirect('/manage/news'),
   async (req, res, next) => {
     try {
       const newsId = req.params.newsId;
-      const newsList = await News.find().sort({ createdAt: -1 });
-      if (newsId) {
-        const news = await News.findOne({ number: newsId });
-        if (!news) {
-          return res.redirect('/manage/news');
-        }
-        res.render('manage/news', {
-          activity: 'manage-page-editnews',
-          news: news,
-          newsList: newsList,
-          title: 'Edit news'
-        });
-      } else {
-        res.render('manage/news', {
-          activity: 'manage-page-addnews',
-          newsList: newsList,
-          title: 'Add news'
-        });
+      const news = await News.findOne({ number: newsId });
+      if (!news) {
+        return res.redirect('/manage/news');
       }
+      res.render('manage/news', {
+        activity: 'manage-page-editnews',
+        news: news,
+        title: 'Edit news',
+        crud: 'update',
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get('/news/create',
+  authRequired,
+  async (req, res, next) => {
+    try {
+      res.render('manage/news', {
+        activity: 'manage-page-addnews',
+        title: 'Add news',
+        crud: 'create',
+      });
     } catch (err) {
       next(err);
     }
