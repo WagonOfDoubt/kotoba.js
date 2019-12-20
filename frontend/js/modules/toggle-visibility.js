fb2e9d0af3dbda160a96703291335ce4fb1cde2a
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2019-12-20 07:20:20 +0300

    Extend toggle-visibility script

--- frontend/js/modules/toggle-visibility.js
+++ frontend/js/modules/toggle-visibility.js
@@ -11,9 +11,28 @@ import $ from 'jquery';
 export const initToggleVisibility = () => {
   $('body').on('click', '.js-toggle-visibility', e => {
     const btn = e.currentTarget;
-    btn.dataset.visible = btn.dataset.visible === 'true' ? 'false' : 'true';
-    btn.innerHTML = btn.dataset.visible === 'true' ? 'hide' : 'show';
-    $(btn.dataset.target).toggleClass('hidden', btn.dataset.visible !== 'true');
+    const isVisible = btn.dataset.visible === 'false';
+    if (isVisible && btn.dataset.group) {
+      $(btn.dataset.group).each((i, el) => {
+        el.dataset.visible = 'false';
+      });
+    }
+    btn.dataset.visible = isVisible ? 'true' : 'false';
+    if (btn.dataset.captionHide && isVisible) {
+      btn.innerHTML = btn.dataset.captionHide;
+    }
+    if (btn.dataset.captionShow && !isVisible) {
+      btn.innerHTML = btn.dataset.captionShow;
+    }
+    if (btn.dataset.target) {
+      $(btn.dataset.target).toggleClass('hidden', !isVisible);
+    }
+    if (isVisible && btn.dataset.hide) {
+      $(btn.dataset.hide).toggleClass('hidden', true);
+    }
+    if (isVisible && btn.dataset.show) {
+      $(btn.dataset.show).toggleClass('hidden', false);
+    }
     e.preventDefault();
   });
 };
